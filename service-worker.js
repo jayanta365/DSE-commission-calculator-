39923be412bf5e1a69bbc1488b7bd23f0de7752c
Jayanta Boruah <Jayanta365@gmail.com>
2026-08-30 20:04:57 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = "dse-calculator-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+
+self.addEventListener("install", function(event) {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+
+                return cache.addAll(APP_FILES);
+
+            })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", function(event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function(keys) {
+
+            return Promise.all(
+
+                keys
+                    .filter(function(key) {
+
+                        return key !== CACHE_NAME;
+
+                    })
+                    .map(function(key) {
+
+                        return caches.delete(key);
+
+                    })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", function(event) {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(function(response) {
+
+                if (response) {
+
+                    return response;
+
+                }
+
+                return fetch(event.request);
+
+            })
+
+    );
+
+});
